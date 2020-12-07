8acd3eb2158e98dabfedf42efddc7e80eed9b494
SergeM <sbmatyunin@gmail.com>
2020-12-07 21:04:25 +0100

    draft

--- flower-search/app.py
+++ flower-search/app.py
@@ -15,11 +15,12 @@ RANDOM_SEED = 14
 def config():
     os.environ['PARALLEL'] = str(4)
     os.environ['SHARDS'] = str(2)
-    os.environ['TMP_DATA_DIR'] = '/tmp/jina/flower'
+    os.environ['DATA_DIR'] = os.environ.get('DATA_DIR', './data/jpg')
     os.environ['COLOR_CHANNEL_AXIS'] = str(0)
     os.environ['JINA_PORT'] = str(45678)
     os.environ['ENCODER'] = os.environ.get('ENCODER', 'jinaai/hub.executors.encoders.image.torchvision-mobilenet_v2')
-    os.environ['WORKDIR'] = os.environ.get('WORKDIR', get_random_ws(os.environ['TMP_DATA_DIR']))
+    os.environ['WORKDIR'] = os.environ.get('WORKDIR', './data/workdir')
+
 
 def get_random_ws(workspace_path, length=8):
     random.seed(RANDOM_SEED)
@@ -33,18 +34,22 @@ def get_random_ws(workspace_path, length=8):
 @click.option('--num_docs', '-n', default=50)
 def main(task, num_docs):
     config()
-    data_path = os.path.join(os.environ['TMP_DATA_DIR'], 'jpg')
+    print("ENCODER", os.environ['ENCODER'])
+    data_path = os.environ['DATA_DIR']
+    print("data_path {}".format(data_path))
     if task == 'index':
         workspace = os.environ['WORKDIR']
+        print("workspace {}".format(workspace))
         if os.path.exists(workspace):
-            print(f'\n +---------------------------------------------------------------------------------+ \
-                    \n |                                   🤖🤖🤖                                        | \
-                    \n | The directory {workspace} already exists. Please remove it before indexing again. | \
-                    \n |                                   🤖🤖🤖                                        | \
-                    \n +---------------------------------------------------------------------------------+')
+            print(f'The directory {workspace} already exists. Please remove it before indexing.')
+            exit(1)
+        print("loading config")
         f = Flow().load_config('flow-index.yml')
+        print("flow created")
         with f:
+            print("indexing")
             f.index_files(f'{data_path}/*.jpg', size=num_docs, read_mode='rb', batch_size=2)
+        print("indexing done")
     elif task == 'query':
         f = Flow().load_config('flow-query.yml')
         f.use_rest_gateway()
